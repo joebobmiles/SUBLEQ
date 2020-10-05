4f6eaf33f298c8475190dc13b36797a90d6131c0
Joseph R Miles <joe@jrm.dev>
2020-10-04 19:42:08 -0700

    Added test to verify that status NORMAL is returned for an empty program

--- test/libsubleq/main.c
+++ test/libsubleq/main.c
@@ -3,4 +3,11 @@
 
 // TODO
 
+UTEST(subleq_execute, returns_status_normal_for_empty_program)
+{
+    ASSERT_EQ(
+        SUBLEQ_STATUS_NORMAL,
+        subleq_execute(NULL, 0, &(subleq_execute_options) {}));
+}
+
 UTEST_MAIN();
