d665de456d41f372bb25a44010904e42fc2718f0
Joseph R Miles <joe@jrm.dev>
2020-10-04 19:24:01 -0700

    Resetting to write better tests

--- include/libsubleq.h
+++ include/libsubleq.h
@@ -28,11 +28,6 @@ subleq_execute(
     if (program_size % 3 != 0)
         return SUBLEQ_STATUS_INVALID_PROGRAM;
 
-    while ((options->instruction_count--) > 0)
-    {
-        program[1] = program[0] - program[1];
-    }
-
     return SUBLEQ_STATUS_NORMAL;
 }
 
--- test/libsubleq/main.c
+++ test/libsubleq/main.c
@@ -25,29 +25,6 @@ UTEST(_, returns_invalid_program_when_size_is_not_multiple_of_three)
             &(subleq_execute_options){ }));
 }
 
-UTEST(_, no_op_program_does_not_change)
-{
-    double program[] = { 0, 0, -1 };
 
-    subleq_execute(program, sizeof(program), &(subleq_execute_options){ });
-
-    ASSERT_EQ(0, program[0]);
-    ASSERT_EQ(0, program[1]);
-    ASSERT_EQ(-1, program[2]);
-}
-
-UTEST(_, subtracts_A_from_B_and_stores_in_B)
-{
-    double program[] = { 1, 1, -1 };
-
-    subleq_execute(
-        program,
-        sizeof(program),
-        &(subleq_execute_options){ .instruction_count = 1 });
-
-    ASSERT_EQ(1, program[0]);
-    ASSERT_EQ(0, program[1]);
-    ASSERT_EQ(-1, program[2]);
-}
 
 UTEST_MAIN();
